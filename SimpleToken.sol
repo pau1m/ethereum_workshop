9cd1ef1d4d9776a097ff7f3db353c2cc24108b3d
pau1m <paul@freedomthroughweb.com>
2018-05-01 09:53:12 +0100

    updating tabs and adding emit

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -6,35 +6,35 @@ pragma solidity ^0.4.18;
  */
 contract BasicToken {
 
-  mapping(address => uint256) balances;
+mapping(address => uint) balances;
 
-  uint256 public totalSupply;
+    uint public totalSupply;
 
-  event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
   /**
   * @dev transfer token for a specified address
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
 
-    balances[msg.sender] = balances[msg.sender] -= _value;
-    balances[_to] = balances[_to] += _value;
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
+        balances[msg.sender] = balances[msg.sender] -= _value;
+        balances[_to] = balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 
   /**
   * @dev Gets the balance of the specified address.
   * @param _owner The address to query the the balance of.
   * @return An uint256 representing the amount owned by the passed address.
   */
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
+    }
 }
 
 /**
@@ -42,18 +42,18 @@ contract BasicToken {
  */
 contract SimpleToken is BasicToken {
 
-  string public constant name = "SimpleToken";
-  string public constant symbol = "SIM";
-  uint8 public constant decimals = 18;
+    string public constant name = "WorkshopToken";
+    string public constant symbol = "WTX";
+    uint8 public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+    uint public constant INITIAL_SUPPLY = 10000 * (10 ** uint(decimals));
 
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  function SimpleToken() public {
-    totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
-  }
+    function SimpleToken() public {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    }
 }
