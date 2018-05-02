987556ef9dc76baddfe254bb19063d811f3dd335
pau1m <paul@freedomthroughweb.com>
2018-05-02 11:33:31 +0100

    amending truffle deployment

--- truffle/migrations/1_initial_migration.1.js
+++ truffle/migrations/1_initial_migration.1.js
@@ -1,7 +1,7 @@
-//const Migrations = artifacts.require("./Migrations.sol");
-var SimpleMintableToken = artifacts.require('SimpleMintableToken')
+const Migrations = artifacts.require("Migrations");
+const SimpleMintableToken = artifacts.require('SimpleMintableToken')
 
 module.exports = function(deployer) {
- // deployer.deploy(Migrations);
+  deployer.deploy(Migrations);
   deployer.deploy(SimpleMintableToken)
 }
